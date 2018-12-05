a0300fe8048c0410f6fe19dd6a776bf0f579cfae
Vitaly Kozadayev <vitkoz@gmail.com>
2018-12-05 16:14:52 -0500

    added property to migration

--- db/migrate/20181204032719_add_property_to_renter.rb
+++ db/migrate/20181204032719_add_property_to_renter.rb
@@ -1,6 +1,6 @@
 class AddPropertyToRenter < ActiveRecord::Migration[5.2]
   def change
-    add_reference :people, :property, foreign_key: true
+    add_column :people, :property_id, :integer, foreign_key: true
     add_column :people, :unit, :string
     add_column :people, :rent, :decimal, precision: 8, scale: 2
   end
