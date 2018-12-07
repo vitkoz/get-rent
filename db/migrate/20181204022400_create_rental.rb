265845fecb4939fa02ebe16589eaec5c78aaa16e
Vitaly Kozadayev <vitkoz@gmail.com>
2018-12-07 14:01:58 -0500

    renamed suit to filing

--- db/migrate/20181204022400_create_rental.rb
+++ db/migrate/20181204022400_create_rental.rb
@@ -1,8 +1,8 @@
 class CreateRental < ActiveRecord::Migration[5.2]
   def change
     create_table :rentals do |t|
-      t.references :property, foreign_key: true
-      t.references :renter, foreign_key: true
+      t.integer :property_id, foreign_key: true
+      t.integer :renter_id, foreign_key: true
       t.string :unit
       t.decimal :rent, precision: 8, scale: 2
     end
